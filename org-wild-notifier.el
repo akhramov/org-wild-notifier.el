6e194d0f0a21b7d2b09ebdef5ffcd5ffe3633339
Artem Khramov <akhramov@pm.me>
2019-06-08 10:10:49 +0600

    Release 0.3.1
    This is a minor release which incorporates bug fixes:
    
    - Fix builds on emacs 24.
    - straight.el support (#21)
    - missing agenda files make Emacs hang (#23)

--- org-wild-notifier.el
+++ org-wild-notifier.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artem Khramov <futu.fata@gmail.com>
 ;; Created: 6 Jan 2017
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Package-Requires: ((alert "1.2") (async "1.9.3") (dash "2.13.0") (dash-functional "1.2.0") (emacs "24.4"))
 ;; Keywords: notification alert org org-agenda agenda
 ;; URL: https://github.com/akhramov/org-wild-notifier.el
